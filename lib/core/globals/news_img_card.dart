e9110c23838496f4bd40ca48e918ff12f1fea9da
kangminju <yessuk5499@gmail.com>
2025-05-09 00:09:21 +0900

    NewsImgCard 생송

---
+++ lib/core/globals/news_img_card.dart
@@ -0,0 +1,21 @@
+class NewsImgCard {
+  final String imgRoute;
+  final String newsTitle;
+
+  NewsImgCard({
+    required this.imgRoute,
+    required this.newsTitle
+  });
+}
+
+class NewsImgCardColumn {
+  final String imgRoute;
+  final String newsTitle;
+  final String time;
+
+  NewsImgCardColumn({
+    required this.imgRoute,
+    required this.newsTitle,
+    required this.time,
+  });
+}
