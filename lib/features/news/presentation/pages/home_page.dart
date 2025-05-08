4637469af2e5ace6b442b6d7e2b08388d8797970
kangminju <yessuk5499@gmail.com>
2025-05-08 13:49:58 +0900

    홈 화면 검색, 앱바 설정

--- lib/features/news/presentation/pages/home_page.dart
+++ lib/features/news/presentation/pages/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:danew/core/theme/colors.dart';
+import 'package:danew/core/theme/padding.dart';
+import 'package:danew/core/widgets/app_bar/primary_appbar.dart';
+import 'package:danew/core/widgets/custom_search_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,11 +15,20 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("DANEW")),
+      appBar: PrimaryAppbar(
+        showLogo: true,
+        showNotificationIcon: true,
+      ),
       backgroundColor: AppColors.whiteColor,
       body: SingleChildScrollView(
         child: Column(
+          children: [
+            Padding(
+              padding: AppPadding.h20v8Padding,
+              child: CustomSearchBar(),
+            ),
 
+          ],
         ),
       ),
     );
