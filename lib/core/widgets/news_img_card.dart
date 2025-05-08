a102c18d5528884ad7be173f92757182f25af98e
kangminju <yessuk5499@gmail.com>
2025-05-09 00:09:37 +0900

    NewsImgCard 구현

---
+++ lib/core/widgets/news_img_card.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import '../theme/sizedbox.dart';
+import '../theme/text_styles.dart';
+
+class NewsImgCard extends StatefulWidget {
+  final String imgRoute;
+  final String newsTitle;
+  const NewsImgCard({super.key, required this.imgRoute, required this.newsTitle});
+
+  @override
+  State<NewsImgCard> createState() => _NewsImgCardState();
+}
+
+class _NewsImgCardState extends State<NewsImgCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: InkWell(
+        onTap: () {},
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(16),
+              child: Image.asset(
+                widget.imgRoute,
+                fit: BoxFit.cover,
+                height: 120,
+              ),
+            ),
+            AppSizedBox.h4SizedBox,
+            Text(widget.newsTitle, style: AppTextStyles.medium14,)
+          ],
+        ),
+      ),
+    );
+  }
+}
