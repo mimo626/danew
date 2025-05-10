b31cd688ace0df2d346d79dcc832cf6f28303011
kangminju <yessuk5499@gmail.com>
2025-05-11 01:06:40 +0900

    MyListWidget 생성

---
+++ lib/features/user_management/presentation/widgets/my_list_widget.dart
@@ -0,0 +1,33 @@
+import 'package:danew/core/theme/text_styles.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../core/theme/colors.dart';
+import '../../../../core/theme/padding.dart';
+
+class MyListWidget extends StatelessWidget {
+  final String title;
+  final VoidCallback onTap;
+  final Color color;
+  const MyListWidget({super.key, 
+    required this.title, 
+    required this.onTap, 
+    this.color = Colors.black,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: AppPadding.h20v16Padding,
+            child: Text(title, style: AppTextStyles.medium14.copyWith(color: color),),
+          ),
+          Divider(color: AppColors.lightGreyColor,)
+        ],
+      ),
+    );
+  }
+}
