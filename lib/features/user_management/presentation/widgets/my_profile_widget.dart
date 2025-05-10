575f2da3da8ecf367626c6007169d0baa209c9a7
kangminju <yessuk5499@gmail.com>
2025-05-11 01:09:15 +0900

    MyProfileWidget 생성

---
+++ lib/features/user_management/presentation/widgets/my_profile_widget.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../../core/theme/colors.dart';
+import '../../../../core/theme/padding.dart';
+import '../../../../core/theme/sizedbox.dart';
+
+class MyProfileWidget extends StatelessWidget {
+  const MyProfileWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: AppPadding.h20v16Padding,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              CircleAvatar(
+                backgroundColor: AppColors.secondaryColor,
+                radius: 28,
+                child: SvgPicture.asset("lib/core/icons/user.svg"),
+              ),
+              AppSizedBox.w8SizedBox,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("danew@naver.com"),
+                  AppSizedBox.h8SizedBox,
+                  Text("강민주"),
+                ],
+              )
+            ],
+          ),
+          InkWell(
+              child: SvgPicture.asset("lib/core/icons/pencil.svg")
+          ),
+        ],
+      ),
+    );
+  }
+}
