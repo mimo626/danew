beaa6328473db187f67b207a9ec31a6bae786af6
kangminju <yessuk5499@gmail.com>
2025-05-10 22:40:38 +0900

    검색 페이지 구현

--- lib/features/search/presentation/pages/search_page.dart
+++ lib/features/search/presentation/pages/search_page.dart
@@ -1,5 +1,12 @@
+import 'package:danew/core/theme/colors.dart';
+import 'package:danew/core/theme/sizedbox.dart';
 import 'package:danew/core/widgets/app_bar/search_app_bar.dart';
+import 'package:danew/features/search/presentation/widgets/recent_search_word_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../../core/theme/padding.dart';
+import '../../../../core/theme/text_styles.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -13,8 +20,26 @@ class _SearchPageState extends State<SearchPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: SearchBarAppbar(),
-      body: Column(
-
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: AppPadding.h20v16Padding,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("최근 검색어", style: AppTextStyles.semiBold16,),
+                  InkWell(
+                    onTap: () {},
+                    child: Text("전체 삭제", style: AppTextStyles.medium14.copyWith(color: AppColors.greyColor),),
+                  ),
+                ],
+              ),
+            ),
+            RecentSearchWordList(),
+          ],
+        ),
       ),
     );
   }
