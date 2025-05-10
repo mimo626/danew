31ea938a4fb72800d61d5911ec82b9d07286377c
kangminju <yessuk5499@gmail.com>
2025-05-10 20:48:40 +0900

    home_recommend_tab_view.dart 실시간 맞춤 뉴스 추가

--- lib/features/news/presentation/pages/home_recommend_tab_view.dart
+++ lib/features/news/presentation/pages/home_recommend_tab_view.dart
@@ -2,6 +2,7 @@ import 'package:danew/core/globals/globals.dart';
 import 'package:danew/core/theme/sizedbox.dart';
 import 'package:danew/features/news/presentation/widgets/news_flash_container.dart';
 import 'package:danew/features/news/presentation/widgets/news_img_card.dart';
+import 'package:danew/features/news/presentation/widgets/news_img_card_list.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../core/theme/padding.dart';
@@ -18,40 +19,48 @@ class HomeRecommendTabView extends StatefulWidget {
 class _HomeRecommendTabViewState extends State<HomeRecommendTabView> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        NewsFlashContainer(newsTitle: "AI 챗봇, 병원 예약·진료 안내 서비스에 도입"),
-        AppSizedBox.h12SizedBox,
-        Padding(
-          padding: AppPadding.h20v16Padding,
-          child: Text("민주님을 위한 추천 뉴스", style: AppTextStyles.semiBold16,),
-        ),
-        // 뉴스 제목 리스트
-        NewsTitleList(newList: Globals.titleNewsData,),
-        // 뉴스 이미지 카드
-        Padding(
-          padding: AppPadding.h20v12Padding,
-          child: SafeArea(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                NewsImgCard(
-                    imgRoute: Globals.newsImgCardData[0].imgRoute,
-                    newsTitle: Globals.newsImgCardData[0].newsTitle,
-                  onTap: () {},
-                ),
-                AppSizedBox.w16SizedBox,
-                NewsImgCard(
-                    imgRoute: Globals.newsImgCardData[1].imgRoute,
-                    newsTitle: Globals.newsImgCardData[1].newsTitle,
-                  onTap: () {},
-                ),
-              ],
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          NewsFlashContainer(newsTitle: "AI 챗봇, 병원 예약·진료 안내 서비스에 도입"),
+          AppSizedBox.h12SizedBox,
+          Padding(
+            padding: AppPadding.h20v16Padding,
+            child: Text("민주님을 위한 추천 뉴스", style: AppTextStyles.semiBold18,),
+          ),
+          // 뉴스 제목 리스트
+          NewsTitleList(newsList: Globals.titleNewsData,),
+          // 뉴스 이미지 카드
+          Padding(
+            padding: AppPadding.h20v12Padding,
+            child: SafeArea(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  NewsImgCard(
+                      imgRoute: Globals.newsImgCardData[0].imgRoute,
+                      newsTitle: Globals.newsImgCardData[0].newsTitle,
+                    onTap: () {},
+                  ),
+                  AppSizedBox.w16SizedBox,
+                  NewsImgCard(
+                      imgRoute: Globals.newsImgCardData[1].imgRoute,
+                      newsTitle: Globals.newsImgCardData[1].newsTitle,
+                    onTap: () {},
+                  ),
+                ],
+              ),
             ),
           ),
-        )
-      ],
+          Padding(
+            padding: AppPadding.h20v16Padding,
+            child: Text("실시간 맞춤 뉴스", style: AppTextStyles.semiBold18,),
+          ),
+          // 뉴스 이미지 카드 리스트
+          NewsImgCardList(newsList: Globals.newsImgCardColumnData,)
+        ],
+      ),
     );
   }
 }
