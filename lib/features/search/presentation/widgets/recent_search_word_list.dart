9bbc9465cf659a645b84538faa14f2cf4b1ea383
kangminju <yessuk5499@gmail.com>
2025-05-10 22:40:20 +0900

    RecentSearchWordList 구현

---
+++ lib/features/search/presentation/widgets/recent_search_word_list.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../../core/globals/globals.dart';
+import '../../../../core/theme/colors.dart';
+import '../../../../core/theme/padding.dart';
+import '../../../../core/theme/text_styles.dart';
+
+class RecentSearchWordList extends StatefulWidget {
+  const RecentSearchWordList({super.key});
+
+  @override
+  State<RecentSearchWordList> createState() => _RecentSearchWordListState();
+}
+
+class _RecentSearchWordListState extends State<RecentSearchWordList> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: AppPadding.h20Padding,
+      child: Expanded(
+        child: ListView.builder(
+          shrinkWrap: true,
+          itemCount: Globals.recentSearchWordList.length,
+          itemBuilder: (context, index) {
+            final newsIndex = Globals.recentSearchWordList[index];
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: InkWell(
+                        onTap: () {},
+                          child: Text(newsIndex, style: AppTextStyles.medium14,)),
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Padding(
+                        padding: AppPadding.all8Padding,
+                        child: SizedBox(
+                          width: 16,
+                          height: 16,
+                          child: SvgPicture.asset(
+                            "lib/core/icons/x.svg",
+                            color: AppColors.greyColor,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Divider(color: AppColors.lightGreyColor,)
+              ],
+            );
+          },
+        ),
+      ),
+    );;
+  }
+}
