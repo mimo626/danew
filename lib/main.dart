7aea64946c25eb1a5ad929f1980560aa33f85719
kangminju <yessuk5499@gmail.com>
2025-05-12 18:41:41 +0900

    GoRouter 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,41 @@
 import 'package:danew/core/theme/colors.dart';
+import 'package:danew/features/login/presentation/pages/additional_user_info_page.dart';
+import 'package:danew/features/login/presentation/pages/login_page.dart';
 import 'package:danew/main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:go_router/go_router.dart';
 
 
 void main() {
   runApp(const MyApp());
 }
+
+/// The route configuration.
+final GoRouter _router = GoRouter(
+  initialLocation: "/login",
+  routes: <RouteBase>[
+    GoRoute(
+    path: '/login',
+      builder: (BuildContext context, GoRouterState state) {
+        return const LoginPage();
+      },
+    ),
+    GoRoute(
+      path: '/additionalUserInfo',
+      builder: (BuildContext context, GoRouterState state) {
+        return const AdditionalUserInfoPage();
+      },
+    ),
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const MainPage();
+      },
+    ),
+  ],
+);
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -14,15 +43,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        fontFamily: 'Pretendard',
-        scaffoldBackgroundColor: Colors.white,
-        appBarTheme: const AppBarTheme(backgroundColor: Colors.white),
-        useMaterial3: true,
-      ),
-      home: const MainPage(),
+    return MaterialApp.router(
+      routerConfig: _router,
     );
   }
 }
