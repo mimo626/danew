3284accce65c38943b43cba474fb4b4ca66dd746
kangminju <yessuk5499@gmail.com>
2025-05-19 04:13:07 +0900

    최근 본 뉴스 구현

--- lib/features/diary/presentation/pages/diary_page.dart
+++ lib/features/diary/presentation/pages/diary_page.dart
@@ -1,6 +1,11 @@
+import 'package:danew/core/globals/globals.dart';
+import 'package:danew/features/diary/presentation/widgets/news_medium_img_card_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 import '../../../../core/theme/colors.dart';
+import '../../../../core/theme/padding.dart';
+import '../../../../core/theme/text_styles.dart';
 import '../../../../core/widgets/app_bar/primary_appbar.dart';
 
 class DiaryPage extends StatefulWidget {
@@ -20,8 +25,36 @@ class _DiaryPageState extends State<DiaryPage> {
         showNotificationIcon: true,
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          Padding(
+            padding: AppPadding.h20v16Padding,
+            child: Text("최근 본 뉴스", style: AppTextStyles.semiBold18,),
+          ),
+          Padding(
+            padding: AppPadding.h20Padding,
+            child: Container(
+              height: 100,
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal, // 가로 스크롤
+                itemCount: Globals.newsMediumImgCardData.length,
+                itemBuilder: (context, index) {
+                  final item = Globals.newsMediumImgCardData[index];
+                  return Padding(
+                    padding: AppPadding.right12Padding,
+                    child: NewsMediumImgCardWidget(
+                      imgRoute: item.imgRoute,
+                      newsTitle: item.newsTitle,
+                      onTap: () {
 
+                      },
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
         ],
       ),
     );
