d9d3375c95de238b7b536bd489e6a429022df4b6
kangminju <yessuk5499@gmail.com>
2025-05-20 02:05:35 +0900

    NewsData 생성

---
+++ lib/core/globals/news_data.dart
@@ -0,0 +1,19 @@
+class NewsData {
+  final String imgUrl;
+  final String newsTitle;
+  final String newsContent;
+  final String pubDate;
+  final String creator;
+  final String category;
+  final String link;
+
+  NewsData({
+    required this.imgUrl,
+    required this.newsTitle,
+    required this.newsContent,
+    required this.pubDate,
+    required this.creator,
+    required this.category,
+    required this.link,
+  });
+}
