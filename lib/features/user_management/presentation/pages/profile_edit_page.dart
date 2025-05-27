aaa212cbb7372ce35b161bede0dde8a9536e9f6b
kangminju <yessuk5499@gmail.com>
2025-05-27 16:55:36 +0900

    프로필 수정 화면 생성

---
+++ lib/features/user_management/presentation/pages/profile_edit_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/theme/colors.dart';
+import '../../../../core/widgets/app_bar/primary_appbar.dart';
+
+class ProfileEditPage extends StatefulWidget {
+  const ProfileEditPage({super.key});
+
+  @override
+  State<ProfileEditPage> createState() => _ProfileEditPageState();
+}
+
+class _ProfileEditPageState extends State<ProfileEditPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.whiteColor,
+      appBar: PrimaryAppbar(
+        title: "프로필 수정",
+      ),
+      body: Column(
+        children: [
+        ],
+      ),
+    );
+  }
+}
