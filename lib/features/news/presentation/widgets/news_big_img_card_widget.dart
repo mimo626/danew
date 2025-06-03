0ee456063f088e7e2d49c94713c1cb5b0dccbfda
kangminju <yessuk5499@gmail.com>
2025-06-03 22:50:25 +0900

    NewsBigImgCardWidget RiverPod 설정, 매개변수 수정

--- lib/features/news/presentation/widgets/news_big_img_card_widget.dart
+++ lib/features/news/presentation/widgets/news_big_img_card_widget.dart
@@ -1,19 +1,21 @@
 import 'package:danew/core/globals/news_data.dart';
+import 'package:danew/features/news/data/model/news_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../../../core/theme/sizedbox.dart';
 import '../../../../core/theme/text_styles.dart';
 
-class NewsBigImgCardWidget extends StatefulWidget {
-  final NewsData newsData;
+class NewsBigImgCardWidget extends ConsumerStatefulWidget {
+  final NewsModel newsData;
   const NewsBigImgCardWidget({super.key, required this.newsData});
 
   @override
-  State<NewsBigImgCardWidget> createState() => _NewsBigImgCardWidgetState();
+  NewsBigImgCardWidgetState createState() => NewsBigImgCardWidgetState();
 }
 
-class _NewsBigImgCardWidgetState extends State<NewsBigImgCardWidget> {
+class NewsBigImgCardWidgetState extends ConsumerState<NewsBigImgCardWidget> {
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -24,14 +26,15 @@ class _NewsBigImgCardWidgetState extends State<NewsBigImgCardWidget> {
           children: [
             ClipRRect(
               borderRadius: BorderRadius.circular(8),
-              child: Image.asset(
-                widget.newsData.imgUrl,
+              //TODO 빈 이미지 채우기
+              child: Image.network(
+                widget.newsData.image_url ?? "",
                 fit: BoxFit.cover,
                 height: 120,
               ),
             ),
             AppSizedBox.h4SizedBox,
-            Text(widget.newsData.newsTitle, style: AppTextStyles.medium14,)
+            Text(widget.newsData.title!, style: AppTextStyles.medium14,)
           ],
         ),
       ),
