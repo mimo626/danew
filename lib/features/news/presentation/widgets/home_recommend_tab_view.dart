604bb23a2c628327c9cea3a626d074221a4eb9a0
kangminju <yessuk5499@gmail.com>
2025-06-03 22:53:23 +0900

    홈 추천 탭 뷰 수정 -> 뉴스 API 적용

--- lib/features/news/presentation/widgets/home_recommend_tab_view.dart
+++ lib/features/news/presentation/widgets/home_recommend_tab_view.dart
@@ -4,68 +4,82 @@ import 'package:danew/features/news/presentation/widgets/news_flash_container_wi
 import 'package:danew/features/news/presentation/widgets/news_big_img_card_widget.dart';
 import 'package:danew/features/news/presentation/widgets/news_small_img_card_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../../core/globals/news_category.dart';
 import '../../../../core/theme/padding.dart';
 import '../../../../core/theme/text_styles.dart';
 import '../../../../core/widgets/list_view_widget.dart';
+import '../providers/newsListProvider.dart';
 import 'news_title_widget.dart';
 
-class HomeRecommendTabView extends StatefulWidget {
-  const HomeRecommendTabView({super.key});
+class HomeRecommendTabView extends ConsumerStatefulWidget {
+  final List<String> categoryList;
+  const HomeRecommendTabView({
+    super.key, required this.categoryList
+  });
 
   @override
-  State<HomeRecommendTabView> createState() => _HomeRecommendTabViewState();
+  HomeRecommendTabViewState createState() => HomeRecommendTabViewState();
 }
 
-class _HomeRecommendTabViewState extends State<HomeRecommendTabView> {
+class HomeRecommendTabViewState extends ConsumerState<HomeRecommendTabView> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          NewsFlashContainer(newsTitle: "AI 챗봇, 병원 예약·진료 안내 서비스에 도입"),
-          AppSizedBox.h12SizedBox,
-          Padding(
-            padding: AppPadding.h20v16Padding,
-            child: Text("민주님을 위한 추천 뉴스", style: AppTextStyles.semiBold18,),
-          ),
-          // 뉴스 제목 리스트
-          ListViewWidget(
-            list: Globals.newsTitleData,
-            listLength: Globals.NewsTItleDataLength,
-            listWidgetBuilder: (item) => NewsTitleWidget(item: item),
-          ),
-          // 뉴스 이미지 카드
-          Padding(
-            padding: AppPadding.h20v12Padding,
-            child: SafeArea(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  NewsBigImgCardWidget(
-                      newsData: Globals.newsBigImgCardData[0],
-                  ),
-                  AppSizedBox.w16SizedBox,
-                  NewsBigImgCardWidget(
-                    newsData: Globals.newsBigImgCardData[1],
-                  ),
-                ],
+    // 카테고리 -> 영어로 바꿔서 api로 전달 후 뉴스 리스트 반환
+    //TODO 나중에 카테고리 리스트로 수정
+    final newsAsync = ref.watch(newsListProvider(NewsCategory.categoryKrToEnTranslate("정치")));
+
+    return newsAsync.when(
+        data: (newsList) => SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              NewsFlashContainer(newsTitle: "AI 챗봇, 병원 예약·진료 안내 서비스에 도입"),
+              AppSizedBox.h12SizedBox,
+              Padding(
+                padding: AppPadding.h20v16Padding,
+                child: Text("민주님을 위한 추천 뉴스", style: AppTextStyles.semiBold18,),
               ),
-            ),
+              // 뉴스 제목 리스트
+              ListViewWidget(
+                list: newsList.sublist(0,3),
+                listLength: Globals.NewsTItleDataLength,
+                listWidgetBuilder: (item) => NewsTitleWidget(item: item),
+              ),
+              // 뉴스 이미지 카드
+              Padding(
+                padding: AppPadding.h20v12Padding,
+                child: SafeArea(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      NewsBigImgCardWidget(
+                        newsData: newsList[3],
+                      ),
+                      AppSizedBox.w16SizedBox,
+                      NewsBigImgCardWidget(
+                        newsData: newsList[4],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: AppPadding.h20v16Padding,
+                child: Text("실시간 맞춤 뉴스", style: AppTextStyles.semiBold18,),
+              ),
+              // 뉴스 이미지 카드 리스트
+              ListViewWidget(
+                  list: newsList.sublist(5),
+                  listLength: newsList.length-5,
+                  listWidgetBuilder: (item) => NewsSmallImgCardWidget(item: item)
+              ),
+            ],
           ),
-          Padding(
-            padding: AppPadding.h20v16Padding,
-            child: Text("실시간 맞춤 뉴스", style: AppTextStyles.semiBold18,),
-          ),
-          // 뉴스 이미지 카드 리스트
-          ListViewWidget(
-              list: Globals.newsSmallImgCardData,
-              listLength: Globals.newsSmallImgCardDataLength,
-              listWidgetBuilder: (item) => NewsSmallImgCardWidget(item: item)
-          ),
-        ],
-      ),
+        ),
+        loading: () => Center(child: const CircularProgressIndicator()),
+        error: (err, _) => Text('에러 발생: $err'),
     );
   }
 }
