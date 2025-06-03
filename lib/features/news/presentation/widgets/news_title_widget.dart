af75ff087cfbfe1d607f739e45d40fc0e8d8768f
kangminju <yessuk5499@gmail.com>
2025-06-03 22:51:20 +0900

    NewsTitleWidget RiverPod 설정, 매개변수 수정

--- lib/features/news/presentation/widgets/news_title_widget.dart
+++ lib/features/news/presentation/widgets/news_title_widget.dart
@@ -1,25 +1,27 @@
 import 'package:danew/core/globals/news_data.dart';
+import 'package:danew/features/news/data/model/news_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import '../../../../core/theme/colors.dart';
 import '../../../../core/theme/text_styles.dart';
 
-class NewsTitleWidget extends StatelessWidget {
+class NewsTitleWidget extends ConsumerWidget {
   const NewsTitleWidget({
     super.key,
     required this.item,
   });
 
-  final NewsData item;
+  final NewsModel item;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         InkWell(
             onTap: () => context.push("/newsDetail", extra: {"newsData": item}),
-            child: Text(item.newsTitle, style: AppTextStyles.medium14,)
+            child: Text(item.title!, style: AppTextStyles.medium14,)
         ),
         Divider(color: AppColors.lightGreyColor,)
       ],
