61263e072bb556b839e3a3cf8418bc6d94804f44
kangminju <yessuk5499@gmail.com>
2025-06-05 02:43:20 +0900

    탭 인디케이터 매개변수 추가

--- lib/core/widgets/tab_widget.dart
+++ lib/core/widgets/tab_widget.dart
@@ -1,3 +1,4 @@
+import 'package:danew/core/theme/padding.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -7,13 +8,15 @@ import '../theme/text_styles.dart';
 class TabWidget extends ConsumerStatefulWidget {
   final List<String> tabTitles;
   final List<Widget> tabViews;
-  final isScrollable;
+  final bool isScrollable;
+  final bool isIndicatorSizeLabel;
 
   const TabWidget({
     super.key,
     required this.tabTitles,
     required this.tabViews,
     required this.isScrollable,
+    this.isIndicatorSizeLabel = true,
   }) : assert(tabTitles.length == tabViews.length, '탭 제목과 내용의 길이가 일치해야 합니다.');
 
   @override
@@ -54,12 +57,12 @@ class TabWidgetState extends ConsumerState<TabWidget> with TickerProviderStateMi
             tabs: widget.tabTitles.map((title) => Tab(text: title)).toList(),
             labelColor: AppColors.blackColor,
             labelStyle: AppTextStyles.medium15,
-            indicatorSize: TabBarIndicatorSize.tab,
+            indicatorSize: widget.isIndicatorSizeLabel ? TabBarIndicatorSize.label : TabBarIndicatorSize.tab,
             indicatorColor: AppColors.blackColor,
             unselectedLabelColor: AppColors.greyColor,
             unselectedLabelStyle: AppTextStyles.medium15,
             tabAlignment: widget.isScrollable ? TabAlignment.start :TabAlignment.fill,
-            dividerColor: Colors.transparent,
+            dividerColor: AppColors.lightGreyColor,
           ),
         ),
         Expanded(
