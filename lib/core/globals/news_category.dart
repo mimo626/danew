710849c20d30dd1b7a461fe372e8fd0bcd78db0c
kangminju <yessuk5499@gmail.com>
2025-06-04 20:16:27 +0900

    NewsCategory 맵으로 수정

--- lib/core/globals/news_category.dart
+++ lib/core/globals/news_category.dart
@@ -1,26 +1,28 @@
 
 class NewsCategory {
-  static String categoryKrToEnTranslate(String categoryKr){
-    late String categoryEn;
-    switch(categoryKr){
-      case("인기"): categoryEn = "top"; break;
-      case("정치"): categoryEn = "politics"; break;
-      case("경제"): categoryEn = "business"; break;
-      case("범죄"): categoryEn = "crime"; break;
-      case("기술"): categoryEn = "technology"; break;
-      case("교육"): categoryEn = "education"; break;
-      case("과학"): categoryEn = "science"; break;
-      case("엔터"): categoryEn = "entertainment"; break;
-      case("환경"): categoryEn = "environment"; break;
-      case("스포츠"): categoryEn = "sports"; break;
-      case("세계"): categoryEn = "world"; break;
-      case("건강"): categoryEn = "health"; break;
-      case("일상"): categoryEn = "lifestyle"; break;
-      case("국내"): categoryEn = "domestic"; break;
-      case("관광"): categoryEn = "tourism"; break;
-      case("음식"): categoryEn = "food"; break;
-      case("기타"): categoryEn = "other"; break;
-    }
-    return categoryEn;
-  }
+  static const Map<String, String> categoryKrToEn = {
+    "인기": "top",
+    "정치": "politics",
+    "경제": "business",
+    "엔터": "entertainment",
+    "스포츠": "sports",
+    "기술": "technology",
+    "과학": "science",
+    "세계": "world",
+    "건강": "health",
+    "일상": "lifestyle",
+  };
+
+  static const Map<String, String> categoryEnToKr = {
+    "top": "인기",
+    "politics": "정치",
+    "business": "경제",
+    "entertainment": "엔터",
+    "sports": "스포츠",
+    "technology": "기술",
+    "science": "과학",
+    "world": "세계",
+    "health": "건강",
+    "lifestyle": "일상",
+  };
 }
