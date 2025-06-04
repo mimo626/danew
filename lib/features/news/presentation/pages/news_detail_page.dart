6c6de0f7f85c3831a4693fc740fd1fae15e9a70e
kangminju <yessuk5499@gmail.com>
2025-06-04 19:20:24 +0900

    뉴스 상세 페이지 뉴스 데이터 적용

--- lib/features/news/presentation/pages/news_detail_page.dart
+++ lib/features/news/presentation/pages/news_detail_page.dart
@@ -1,9 +1,12 @@
 import 'package:danew/core/globals/news_data.dart';
+import 'package:danew/core/intl/date_formatter.dart';
 import 'package:danew/core/theme/padding.dart';
 import 'package:danew/core/theme/sizedbox.dart';
 import 'package:danew/core/theme/text_styles.dart';
 import 'package:danew/core/widgets/button/bookmark_toggle_btn.dart';
 import 'package:danew/core/widgets/button/share_btn_widget.dart';
+import 'package:danew/core/widgets/list_view_widget.dart';
+import 'package:danew/features/news/data/model/news_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -11,7 +14,7 @@ import '../../../../core/theme/colors.dart';
 import '../../../../core/widgets/app_bar/primary_appbar.dart';
 
 class NewsDetailPage extends ConsumerStatefulWidget {
-  final NewsData newsData;
+  final NewsModel newsData;
   const NewsDetailPage({super.key, required this.newsData});
 
   @override
@@ -24,63 +27,75 @@ class NewsDetailPageState extends ConsumerState<NewsDetailPage> {
     return Scaffold(
         backgroundColor: AppColors.whiteColor,
         appBar: PrimaryAppbar(),
-      body: Padding(
-        padding: AppPadding.h20Padding,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            AppSizedBox.h16SizedBox,
-            Container(
-              decoration: BoxDecoration(
-                color: AppColors.tertiaryColor,
-                borderRadius: BorderRadius.circular(20)
-              ),
-              child: Padding(
-                padding: AppPadding.h12v6Padding,
-                child: Text(widget.newsData.category, style: AppTextStyles.medium14.copyWith(color: AppColors.primaryColor),),
-              ),
-            ),
-            AppSizedBox.h12SizedBox,
-            Text(widget.newsData.newsTitle, style: AppTextStyles.medium18,),
-            AppSizedBox.h12SizedBox,
-            Text(widget.newsData.pubDate, style: AppTextStyles.medium14.copyWith(color: AppColors.greyColor),),
-            AppSizedBox.h4SizedBox,
-            Text("${widget.newsData.creator} 기자", style: AppTextStyles.medium14),
-            AppSizedBox.h16SizedBox,
-            Row(
-              children: [
-                Spacer(),
-                BookmarkToggleBtn(),
-                AppSizedBox.w16SizedBox,
-                ShareBtnWidget()
-              ],
-            ),
-            AppSizedBox.h16SizedBox,
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text("AI 뉴스 요약본", style: AppTextStyles.medium14.copyWith(color: AppColors.greyColor),),
-                InkWell(
-                  onTap: () {
-
-                  },
-                  child: Text("뉴스 원문 보기", style: AppTextStyles.medium14.copyWith(
-                      color: AppColors.greyColor,
-                      decoration: TextDecoration.underline,
-                      decorationColor: AppColors.greyColor
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: AppPadding.h20Padding,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              AppSizedBox.h16SizedBox,
+              ListViewWidget(
+                  list: widget.newsData.category,
+                  listLength: widget.newsData.category.length,
+                  listWidgetBuilder: (item) => Padding(
+                    padding: AppPadding.right8Padding,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: AppColors.tertiaryColor,
+                          borderRadius: BorderRadius.circular(20)
+                      ),
+                      child: Padding(
+                        padding: AppPadding.h12v6Padding,
+                        child: Text(item, style: AppTextStyles.medium14.copyWith(color: AppColors.primaryColor),),),
                     ),
                   ),
-                )
-              ],
-            ),
-            AppSizedBox.h12SizedBox,
-            Text(widget.newsData.newsContent, style: AppTextStyles.medium16,),
-            AppSizedBox.h16SizedBox,
-            Image.asset(
-              widget.newsData.imgUrl,
-              height: 250,
-            )
-          ],
+              ),
+              AppSizedBox.h12SizedBox,
+              Text(widget.newsData.title!, style: AppTextStyles.medium18,),
+              AppSizedBox.h12SizedBox,
+              Text(DateFormatter.formatYearMonthDate(widget.newsData.pub_date), style: AppTextStyles.medium14.copyWith(color: AppColors.greyColor),),
+              AppSizedBox.h4SizedBox,
+              ListViewWidget(
+                  list: widget.newsData.category,
+                  listLength: widget.newsData.category.length,
+                  listWidgetBuilder: (item) => Text("${item}, ", style: AppTextStyles.medium14),
+              ),
+              AppSizedBox.h16SizedBox,
+              Row(
+                children: [
+                  Spacer(),
+                  BookmarkToggleBtn(),
+                  AppSizedBox.w16SizedBox,
+                  ShareBtnWidget()
+                ],
+              ),
+              AppSizedBox.h16SizedBox,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("AI 뉴스 요약본", style: AppTextStyles.medium14.copyWith(color: AppColors.greyColor),),
+                  InkWell(
+                    onTap: () {
+        
+                    },
+                    child: Text("뉴스 원문 보기", style: AppTextStyles.medium14.copyWith(
+                        color: AppColors.greyColor,
+                        decoration: TextDecoration.underline,
+                        decorationColor: AppColors.greyColor
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              AppSizedBox.h12SizedBox,
+              Text(widget.newsData.description!, style: AppTextStyles.medium16,),
+              AppSizedBox.h16SizedBox,
+              Image.network(
+                widget.newsData.image_url!,
+                height: 250,
+              )
+            ],
+          ),
         ),
       ),
     );
