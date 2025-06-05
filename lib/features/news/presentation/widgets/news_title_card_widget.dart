35bd1214197955f0db1eae461a66bc2fa4d98bec
kangminju <yessuk5499@gmail.com>
2025-06-05 16:07:47 +0900

    NewsTitleCardWidget 클릭 시 이벤트 설정

--- lib/features/news/presentation/widgets/news_title_card_widget.dart
+++ lib/features/news/presentation/widgets/news_title_card_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../../../core/intl/date_formatter.dart';
 import '../../../../core/theme/colors.dart';
@@ -17,23 +18,26 @@ class NewsTitleCardWidget extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container(
-      width: 240,
-      decoration: BoxDecoration(
-        color: AppColors.lightGreyColor,
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: Padding(
-        padding: AppPadding.h12v8Padding,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(newsModel.title!, style: AppTextStyles.medium16, maxLines: 2,),
-            AppSizedBox.h8SizedBox,
-            Text(DateFormatter.formatYearMonthDate(newsModel.pub_date),
-              style: AppTextStyles.medium12.copyWith(color: AppColors.greyColor),
-            ),
-          ],
+    return InkWell(
+      onTap: () => context.push("/newsDetail", extra: {"newsData": newsModel}),
+      child: Container(
+        width: 240,
+        decoration: BoxDecoration(
+          color: AppColors.lightGreyColor,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Padding(
+          padding: AppPadding.h12v8Padding,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(newsModel.title!, style: AppTextStyles.medium16, maxLines: 2,),
+              AppSizedBox.h8SizedBox,
+              Text(DateFormatter.formatYearMonthDate(newsModel.pub_date),
+                style: AppTextStyles.medium12.copyWith(color: AppColors.greyColor),
+              ),
+            ],
+          ),
         ),
       ),
     );
