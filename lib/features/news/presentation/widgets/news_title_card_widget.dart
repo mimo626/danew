1556c25cf699888f01c737c24bdbdeeb27480587
kangminju <yessuk5499@gmail.com>
2025-06-05 15:27:48 +0900

    NewsTitleCardWidget 생성

---
+++ lib/features/news/presentation/widgets/news_title_card_widget.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../../core/intl/date_formatter.dart';
+import '../../../../core/theme/colors.dart';
+import '../../../../core/theme/padding.dart';
+import '../../../../core/theme/sizedbox.dart';
+import '../../../../core/theme/text_styles.dart';
+import '../../data/model/news_model.dart';
+
+class NewsTitleCardWidget extends ConsumerWidget {
+  final NewsModel newsModel;
+  const NewsTitleCardWidget({
+    super.key,
+    required this.newsModel,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container(
+      width: 240,
+      decoration: BoxDecoration(
+        color: AppColors.lightGreyColor,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Padding(
+        padding: AppPadding.h12v8Padding,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(newsModel.title!, style: AppTextStyles.medium16, maxLines: 2,),
+            AppSizedBox.h8SizedBox,
+            Text(DateFormatter.formatYearMonthDate(newsModel.pub_date),
+              style: AppTextStyles.medium12.copyWith(color: AppColors.greyColor),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
