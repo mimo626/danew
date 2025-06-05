470be85d11febd8972011e9f3d786005de3c2b27
kangminju <yessuk5499@gmail.com>
2025-06-05 16:07:09 +0900

    다크 그레이 색 추가

--- lib/core/theme/colors.dart
+++ lib/core/theme/colors.dart
@@ -10,6 +10,7 @@ class AppColors {
   static const Color whiteColor = Color(0xFFFFFFFF);
   static const Color blackColor = Color(0xFF000000);
   static const Color greyColor = Color(0xFF9BA5B7);
+  static const Color darkGreyColor = Color(0xFF3D3D3D);
   static const Color lightGreyColor = Color(0xFFEEF1F4);
   static const Color blueColor = Color(0xFF002AFC);
   static const Color redColor = Color(0xFFFC0032);
